1b4db877f6c39f2f21b61d2f15f084a74f2d8a4a
Kourtney <barnes.kourt@gmail.com>
2022-03-08 15:19:47 -0500

    new

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+    <h4 className="center">About</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequuntur eligendi eum? Consequuntur, nemo quae.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+    <h4 className="center">Contact</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequuntur eligendi eum? Consequuntur, nemo quae.</p>
+    </div>
+  )
+}
+
+export default Contact
