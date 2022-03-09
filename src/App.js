5ef7e56fe576483d7698baed3dfb1f7df4685e79
Tyrone <natiboigaming@gmail.com>
2022-03-09 10:25:37 -0500

    pull

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-
-import Navbar from './components/Navbar';
-import Contact from './components/Contact';
-=======
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
->>>>>>> f2a6cf76f9ad1f9b29560893fb74dc2cb0e284dd
 
 class App extends Component {
   render() {
