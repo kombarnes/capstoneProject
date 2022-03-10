9c22e9866476312a81d5b63b7b6571138d18c8f8
Kourtney <barnes.kourt@gmail.com>
2022-03-10 18:20:46 -0500

    cards

--- backend/models/attraction.model.js
+++ backend/models/attraction.model.js
@@ -3,43 +3,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const attractionSchema = new Schema({
-        name: {
-          type:  String
-        },
-        imageUrl:{
-            type: String
-        } ,
-        description: {
-           type: String
-        },
-        location:{
-            type: locationSchema
-        } 
-
-    
+        name: {type:  String},
+        imageUrl:{type: String} ,
+        description: {type: String},
+        location:{type: locationSchema}
 });
 
 
 
 const locationSchema = new Schema({
-    location: {
-        type: String
-    },
-    address: {
-        type: String
-        },
-    city: {
-        type: String
-    },
-    state: {
-        type: String
-    },
-    zipcode: {
-        type: String
-    }
-    
+    location: {type: String},
+    address: {type: String},
+    city: {type: String},
+    state: {type: String},
+    zipcode: {type: String}
 });
 
-const Attraction = mongoose.model('Attraction', attractionSchema);
+const Attraction = mongoose.model('attraction', attractionSchema);
 
 module.exports = Attraction;
--- src/components/Attractions.js
+++ src/components/Attractions.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import Attraction from '../../backend/models/attraction.model';
+import Cards from './Cards'
 
 const Attractions = () => {
 
   return (
-      <div>
-        <h1 className="center">Visit Cincinnati!</h1>
-        <br />
-        <h5 className='center'>Are you on a family vacation? Maybe you just need time to get away. Our city has options for everyone! How would you like to plan your trip?</h5>
-        <h1 className='center'>Restaurants</h1>
-        
-      </div>
+    <div>
+      <h1 className="center">Visit Cincinnati!</h1>
+      <br />
+      <h5 className='center'>Are you on a family vacation? Maybe you just need time to get away. Our city has options for everyone! How would you like to plan your trip?</h5>
+      <h1 className='center'>Restaurants</h1>
+      <h1 className='center'>Arts/Music</h1>
+      <h1 className='center'>Sports</h1>
+      <h1 className='center'>Amusements/Theme Parks</h1>
+      <h1 className='center'>Night Life</h1>
+    </div>
   )
 }
 
