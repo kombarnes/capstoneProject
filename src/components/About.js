3d04c6a58c3751ea321a253117a6a19078e53c1c
Tyrone <natiboigaming@gmail.com>
2022-03-11 07:54:25 -0500

    pull

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ const About = () => {
     <h4 className="center">The City Of Cincinnati</h4>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequuntur eligendi eum? Consequuntur, nemo quae.</p>
     </div>
+    
   )
 }
 
