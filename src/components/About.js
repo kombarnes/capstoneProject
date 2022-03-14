331167c2bd7e677ddcf3c293452885e806d5d624
Tyrone <natiboigaming@gmail.com>
2022-03-14 10:30:58 -0400

    push

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import "./about.css"
 
 const About = () => {
   return (
     <div>
     <h4 className="center">The City Of Cincinnati</h4>
+    <div clasName="img">
+    <span>
+          <img className="img1" src="https://c8.alamy.com/comp/AHPJNB/riverboats-loading-cargo-at-the-ohio-river-levee-in-cincinnati-1860s-AHPJNB.jpg" alt="" />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequuntur eligendi eum? Consequuntur, nemo quae.</p>
+
+    <img className='img2' src="https://3nwec1qd4zy21zftr339bla3-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/02_Smale-Riverfront-Park_website-1800x1196.jpg" alt="" />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque praesentium, vel iusto voluptates repellendus natus.</p>
+    </span>
+  
+    </div>
+    
     </div>
     
   )
