2a82b031c969784aeb8eaba8f04133afb9bec13b
Tyrone <natiboigaming@gmail.com>
2022-03-17 09:14:00 -0400

    push

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./about.css"
 
 const About = () => {
   return (
     <div>
-    <h4 className="center">The City Of Cincinnati</h4>
-    <div clasName="img">
-    
-          <img className="img1" src="https://c8.alamy.com/comp/AHPJNB/riverboats-loading-cargo-at-the-ohio-river-levee-in-cincinnati-1860s-AHPJNB.jpg" alt="" />
-    
+      <h1 className="center">👑The Queen City known as Cincinnati👑</h1>
 
-    <img className='img2' src="https://3nwec1qd4zy21zftr339bla3-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/02_Smale-Riverfront-Park_website-1800x1196.jpg" alt="" />
 
-    
-  
+      
+
+      <div className="container">
+      <div className="row">
+
+    <div className="col-9 ">
+
+
+          <img className="img1" src="https://www.artworkscincinnati.org/media/Miami_and_Erie_Canal_slide.jpg" alt="" />
+
+
+          <img className='img2' src="https://3nwec1qd4zy21zftr339bla3-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/02_Smale-Riverfront-Park_website-1800x1196.jpg" alt="" />
+
+          <br />
+
+          <img className="img3" src="https://freedomcenter.org/wp-content/uploads/2020/07/JM8_2733a.jpg" alt="" />
+
+
+          <img className='img4' src="https://3nwec1qd4zy21zftr339bla3-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/02_Smale-Riverfront-Park_website-1800x1196.jpg" alt="" />
+
+
+        
     </div>
-    
+    <div className="col-3">
+
+      <div className="info">
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+      </div>
+      </div>
     </div>
-    
+    </div>
+      </div>
+      
+
   )
 }
 
