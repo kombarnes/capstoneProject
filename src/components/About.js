4e0ecf9913835bf0819f587ef0f7edef2e8cce9c
Tyrone <natiboigaming@gmail.com>
2022-03-21 13:24:17 -0400

    push

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,14 @@ const About = () => {
       <h1 className="center">👑The Queen City known as Cincinnati👑</h1>
 
 
+      <div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  {/* <h1 class="my-4"> */}
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
       
 
       <div className="container">
