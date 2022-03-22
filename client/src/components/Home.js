dd82a716622563ccd7e81ee41c74fd1047b40450
Kourtney <barnes.kourt@gmail.com>
2022-03-22 10:13:37 -0400

    push

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import heroVideo from '../videos/cincinnati.mp4'
 
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
@@ -9,7 +10,7 @@ const Navbar = () => {
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
-                <li><a href="/attractions">Attractions</a></li>
+                <li><Link to="/attractions">Attractions</Link></li>
             </ul>
         </div>
     </nav>
