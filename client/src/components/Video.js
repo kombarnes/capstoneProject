d1e02ca7b78399a11549b96cde3b69b0fe798a3b
Kourtney <barnes.kourt@gmail.com>
2022-03-24 11:03:35 -0400

    pushing to main

---
+++ client/src/components/Video.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Video = () => {
+    
+    <div>
+    
+    <h4 className="center">Home</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequuntur eligendi eum? Consequuntur, nemo quae.</p>
+    
+    </div>
+
+
+
+
+     
+}
+
+export default Video;
